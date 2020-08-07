6d574671d3a5777d277534f5feb77002e6c2666a
Roger Bisson <rwbisson@resolvix.com>
2020-08-08 00:24:28 +0100

    Initial work on JndiUtils, JndiUtilsUT

---
+++ src/main/java/com/resolvix/lib/jndi/JndiUtils.java
@@ -0,0 +1,99 @@
+package com.resolvix.lib.jndi;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+public class JndiUtils {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(JndiUtils.class);
+
+    private static final String JAVA_NAMESPACE_PREFIX = "java:";
+
+    private static final String JDNI_COMPONENT_ENVIRONMENT_NAMESPACE = "java:comp/env";
+
+    private static final char JAVA_SEPARATOR = ':';
+
+    private static final char CONTEXT_SEPARATOR = '/';
+
+    private JndiUtils() {
+        //
+    }
+
+    private static String getNamespace(String name) {
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        if (name.startsWith(JAVA_NAMESPACE_PREFIX)) {
+            i = name.indexOf(CONTEXT_SEPARATOR);
+            if (i >= 0) {
+                sb.append(name.substring(0, i));
+                i++;
+            } else {
+                sb.append(name);
+            }
+        } else {
+            sb.append(JDNI_COMPONENT_ENVIRONMENT_NAMESPACE);
+        }
+
+        while (i >= 0 && i < name.length()) {
+            int j = name.indexOf(CONTEXT_SEPARATOR, i);
+            if (j > i) {
+                sb.append(CONTEXT_SEPARATOR);
+                sb.append(name.substring(i, j));
+                i = ++j;
+            } else break;
+        }
+
+        return sb.toString();
+    }
+
+    private static String getName(String name) {
+        int i = name.lastIndexOf(CONTEXT_SEPARATOR);
+        if (i >= 0)
+            return name.substring(i + 1);
+        return name;
+    }
+
+    private static Context getContext(String namespace) throws NamingException {
+        Context context = new InitialContext();
+        LOGGER.debug("InitialContext = {}", context);
+        int i = 0, j = 0;
+        i = namespace.indexOf(JAVA_SEPARATOR);
+        j = namespace.indexOf(CONTEXT_SEPARATOR, i);
+        while (j > i) {
+            String tmp = namespace.substring(i + 1, j);
+            context = (Context) context.lookup(tmp);
+            i = j;
+            j = namespace.indexOf(CONTEXT_SEPARATOR, i + 1);
+        }
+
+        if (i < namespace.length()) {
+            String tmp = namespace.substring(i + 1);
+            context = (Context) context.lookup(tmp);
+        }
+
+        return context;
+    }
+
+    public static <T> void bind(String compoundName, T t) throws NamingException {
+        String namespace = getNamespace(compoundName);
+        Context context = getContext(namespace);
+        if (context != null) {
+            String name = getName(compoundName);
+            context.bind(name, t);
+        }
+    }
+
+    public static <T> T lookup(String compoundName, Class<T> classT) throws NamingException {
+        String namespace = getNamespace(compoundName);
+        Context context = getContext(namespace);
+        if (context != null) {
+            String name = getName(compoundName);
+            return (T) context.lookup(name);
+        } else
+            return null;
+    }
+}
---
+++ src/test/java/com/resolvix/lib/jndi/JndiUtilsUT.java
@@ -0,0 +1,142 @@
+package com.resolvix.lib.jndi;
+
+import org.junit.*;
+import org.junit.rules.ExpectedException;
+import org.powermock.reflect.Whitebox;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+
+public class JndiUtilsUT {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(JndiUtilsUT.class);
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        Context rootContext = new InitialContext();
+        Context compContext = rootContext.createSubcontext("comp");
+        Context envContext = compContext.createSubcontext("env");
+        envContext.bind("readOnly", true);
+        Context jdbcContext = envContext.createSubcontext("jdbc");
+        LOGGER.debug("InitialContext = {}", rootContext);
+    }
+
+    @Before
+    public void before() throws Exception {
+
+    }
+
+    @After
+    public void after() throws Exception {
+
+    }
+
+    //
+    //  getNamespace
+    //
+
+    @Test
+    public void getNamespaceGivenJndiNamingEnvironmentOnly() throws Exception {
+        String namespace = Whitebox.invokeMethod(
+            JndiUtils.class, "getNamespace", "java:comp");
+        assertThat(namespace, equalTo("java:comp"));
+    }
+
+    @Test
+    public void getNamespaceGivenJndiNamingEnvironmentOnlyX() throws Exception {
+        String namespace = Whitebox.invokeMethod(
+            JndiUtils.class, "getNamespace", "java:comp/env");
+        assertThat(namespace, equalTo("java:comp"));
+    }
+
+    @Test
+    public void getNamespaceGivenFullyQualifiedJndiName() throws Exception {
+        String namespace = Whitebox.invokeMethod(
+            JndiUtils.class, "getNamespace", "java:comp/env/jdbc/TEST");
+        assertThat(namespace, equalTo("java:comp/env/jdbc"));
+    }
+
+    @Test
+    public void getNamespaceGivenRelativeJndiName() throws Exception {
+        String namespace = Whitebox.invokeMethod(
+            JndiUtils.class, "getNamespace", "jdbc/TEST");
+        assertThat(namespace, equalTo("java:comp/env/jdbc"));
+    }
+
+    //
+    //  getName
+    //
+
+    @Test
+    public void getNameGivenFullyQualifiedJndiName() throws Exception {
+        String name = Whitebox.invokeMethod(
+            JndiUtils.class, "getName", "java:com/env/jdbc/TEST");
+        assertThat(name, equalTo("TEST"));
+    }
+
+    @Test
+    public void getNameGivenRelativeJndiNamespaceName() throws Exception {
+        String name = Whitebox.invokeMethod(
+            JndiUtils.class, "getName", "jdbc/TEST");
+        assertThat(name, equalTo("TEST"));
+    }
+
+    @Test
+    public void getNameGivenRelativeJndiName() throws Exception {
+        String name = Whitebox.invokeMethod(
+            JndiUtils.class, "getName", "TEST");
+        assertThat(name, equalTo("TEST"));
+    }
+
+    //
+    //  getContext
+    //
+
+    @Test
+    public void getContextCompEnv() throws Exception {
+        Context context = Whitebox.invokeMethod(
+            JndiUtils.class, "getContext", "java:comp/env");
+        assertThat(context, instanceOf(Context.class));
+        assertThat(context.getNameInNamespace(), equalTo("comp.env"));
+    }
+
+    @Test
+    public void getContextCompEnvJdbc() throws Exception {
+        Context context = Whitebox.invokeMethod(
+            JndiUtils.class, "getContext", "java:comp/env/jdbc");
+        assertThat(context, instanceOf(Context.class));
+        assertThat(context.getNameInNamespace(), equalTo("comp.env.jdbc"));
+    }
+
+    @Test
+    public void getContextCompEnvJdbcFailure() throws Exception {
+        expectedException.expect(NamingException.class);
+        Context context = Whitebox.invokeMethod(
+            JndiUtils.class, "getContext", "java:comp/env/jdbc/failure");
+        assertThat(context, instanceOf(Context.class));
+    }
+
+    //
+    //  bind
+    //
+
+    @Test
+    public void bindCompEnvJdbcDataSource() throws Exception {
+        DataSource dataSource = mock(DataSource.class);
+        JndiUtils.bind("java:comp/env/jdbc/dataSource", dataSource);
+        assertThat(
+            JndiUtils.lookup("java:comp/env/jdbc/dataSource", DataSource.class),
+            sameInstance(dataSource));
+    }
+}
