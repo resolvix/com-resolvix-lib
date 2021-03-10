f82c9ac05cb3c58f192f5a87151d0f455d936c2b
Roger Bisson <rwbisson@yahoo.com>
2021-03-10 09:34:32 +0000

    Added EnumUtils {safeName, safeOrdinal} (#16)
    * Added EnumUtils {safeName, safeOrdinal}
    
    * Added EnumUtilsUT {safeName, safeOrdinal}
    
    * Added EnumUtils#safeMap and UTs

---
+++ src/main/java/com/resolvix/lib/utils/EnumUtils.java
@@ -0,0 +1,28 @@
+package com.resolvix.lib.utils;
+
+import java.util.function.Function;
+
+public class EnumUtils {
+
+    public EnumUtils() {
+        //
+    }
+
+    public static <E extends Enum<E>> String safeName(E e) {
+        if (e == null)
+            return null;
+        return e.name();
+    }
+
+    public static <E extends Enum<E>> Integer safeOrdinal(E e) {
+        if (e == null)
+            return null;
+        return e.ordinal();
+    }
+
+    public static <E extends Enum<E>, R> R safeMap(E e, Function<E, R> map) {
+        if (e == null)
+            return null;
+        return map.apply(e);
+    }
+}
